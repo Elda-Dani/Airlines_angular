bb67de841fe26483ec188510478cfcbda7853666
Elda-Dani <eldadani2001@gmail.com>
2022-10-11 17:07:11 +0530

    air

---
+++ src/app/airlines/airlines.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-airlines',
+  templateUrl: './airlines.component.html',
+  styleUrls: ['./airlines.component.css']
+})
+export class AirlinesComponent implements OnInit {
+
+  constructor() { }
+
+
+airlinesData:any=[]
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AirlinesComponent } from './airlines/airlines.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AirlinesComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AirlinesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
